b1e727895a76267c16dc84669e6767371e11c858
sgacevedo <saraacevedo@bell320-137822.utep.edu>
2015-11-16 13:20:47 -0700

    created manageAccounts.php and UI elements

--- home.php
+++ home.php
@@ -1,52 +1,12 @@
-<!-- Sara's comment -->
-
 <html>
 <head>
-
-	<link rel="stylesheet" href="style.css">
-	
-	<!-- Font Awesome (icons) -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-	
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-	
-	<!-- Latest compiled JavaScript -->
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>	
-	
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <?php
+        require_once 'UI/styleIncludes.php'; ?>
 	<title>Login</title>
 </head>
 <body>
-	<nav class="navbar navbar-default navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="home.php"><i class="fa fa-plane"></i>ASDI Feed Analyzer</a>
-        </div>
-        <ul class="nav navbar-nav">
-            <li id="account" ><a href="#about">Account</a></li>
-            <li id="query" class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Query<span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Create Report</a></li>
-                <li><a href="#">Create Model</a></li>
-              </ul>
-            </li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li id="requestAccount"><button class="btn btn-info" type="submit"><a href="requestAccount.php">Request Account</a></button></li>
-            <li id="signOut">
-            	<form action="home.php" method="post">
-            		<input type="hidden" name="LOGOUT" value="yes" />
-            		<button class="btn btn-link" type="submit"><i class="fa fa-sign-out"></i>Sign Out</button>
-            	</form>
-            </li>
-          </ul>
-      </div>
-    </nav>
+	<?php
+        require_once 'UI/navBar.php'; ?>
 	<form id="loginForm" class="form-signin" action="home.php" method="post">
 		<h2 class="form-signin-heading">Log in</h2>
 		<div id="alertLoginIncorrectCred" class="alert alert-danger">

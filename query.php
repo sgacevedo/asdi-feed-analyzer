2fa371dc688eb15b2bdc6fe2dbf21927d2c0c9b9
Sara Acevedo <sgacevedo@miners.utep.edu>
2015-11-21 18:58:36 -0700

    added approveUser protocol

---
+++ query.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <?php
+            require_once 'UI/styleIncludes.php' ?>
+        <title>ASDI Query</title>
+    </head>
+    <body>
+        <?php
+            require_once 'requires.php';
+            require_once 'UI/navBar.php'; ?>
+
+        <div class="contents">
+        <?php
+            
+            ?>
+
+        </div>
+    </body>
+</html>
